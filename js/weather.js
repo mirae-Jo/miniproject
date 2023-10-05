1902f4df7f442128c53e164c90b3d491c97439ac
coldbrew <minsuk815@naver.com>
2023-10-05 23:09:09 +0900

    시간 오류 수정

--- js/weather.js
+++ js/weather.js
@@ -2,15 +2,15 @@ let today = new Date();
 let month = String(today.getUTCMonth() + 1).padStart(2, '0'); //months from 1-12
 let day = String(today.getUTCDate()).padStart(2, '0');
 let year = today.getUTCFullYear();
-const API_KEY = 'dFutIVbhP9iCO5wH%2Bn0dhh4QasCgcBUBGWoQNFldz8YNlyL8Jo0b32tAknfEA8N9YfCYdZ3O2PrRscDkO0gX1g%3D%3D'
+let time = String(today.getHours() - 1).padStart(2, '0');
 today = `${year}${month}${day}`;
+console.log(time)
+const API_KEY = 'dFutIVbhP9iCO5wH%2Bn0dhh4QasCgcBUBGWoQNFldz8YNlyL8Jo0b32tAknfEA8N9YfCYdZ3O2PrRscDkO0gX1g%3D%3D'
 
 //민석 weather
 $.getJSON(
-    `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${today}&base_time=0000&nx=91&ny=76`,
+    `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${today}&base_time=${time}00&nx=91&ny=76`,
     function (data) {
-        console.log(data.response.body.items.item[3].obsrValue)
-
         let showWeather = data.response.body.items.item[3].obsrValue
 
         if (showWeather >= 30) {
@@ -30,10 +30,8 @@ $.getJSON(
 )
 //미래 weather
 $.getJSON(
-    `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${today}&base_time=0000&nx=63&ny=89`,
+    `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${today}&base_time=${time}00&nx=63&ny=89`,
     function (data) {
-        console.log(data.response.body.items.item[3].obsrValue)
-
         let showWeather = data.response.body.items.item[3].obsrValue
 
         if (showWeather >= 30) {
@@ -53,10 +51,8 @@ $.getJSON(
 )
 //유나 weather
 $.getJSON(
-    `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${today}&base_time=0000&nx=60&ny=121`,
+    `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${today}&base_time=${time}00&nx=60&ny=121`,
     function (data) {
-        console.log(data.response.body.items.item[3].obsrValue)
-
         let showWeather = data.response.body.items.item[3].obsrValue
 
         if (showWeather >= 30) {
